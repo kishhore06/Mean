8e7d027c1b94abb40edf76db6952052e8164465b
Maharaj P S <maharajpsd@gmail.com>
2026-03-17 16:24:34 +0530

    Updated UserController

--- mean/src/main/java/com/blog/mean/controller/UserController.java
+++ mean/src/main/java/com/blog/mean/controller/UserController.java
@@ -1,5 +1,66 @@
 package com.blog.mean.controller;
 
+import com.blog.mean.dto.UserDTO;
+import com.blog.mean.entity.User;
+import com.blog.mean.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 public class UserController {
     
+    private final UserService userService;
+    
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> registerUser(@RequestBody User user) {
+        if (userService.existsByUsername(user.getUsername())) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (userService.existsByEmail(user.getEmail())) {
+            return ResponseEntity.badRequest().build();
+        }
+        UserDTO createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@RequestBody User loginRequest) {
+        try {
+            UserDTO user = userService.getUserByUsername(loginRequest.getUsername());
+            // In a real app, you'd verify password here
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        UserDTO user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        List<UserDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody User user) {
+        UserDTO updatedUser = userService.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
