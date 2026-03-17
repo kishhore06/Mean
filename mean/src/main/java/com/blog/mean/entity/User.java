6a7efb8fdc96acf856cc97705a64806bb0020f3f
Maharaj P S <maharajpsd@gmail.com>
2026-03-17 15:34:09 +0530

    User Entity Added

--- mean/src/main/java/com/blog/mean/entity/User.java
+++ mean/src/main/java/com/blog/mean/entity/User.java
@@ -1,11 +1,20 @@
-package com.blog.application.entity;
+package com.blog.mean.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
