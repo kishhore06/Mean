c25c21c4bdc4898f8368e6022a354151b6123a06
Mousigan Iyyamperumal <mousiganofficial@gmail.com>
2026-03-17 15:34:29 +0530

    post.java added

--- mean/src/main/java/com/blog/mean/entity/Post.java
+++ mean/src/main/java/com/blog/mean/entity/Post.java
@@ -1,5 +1,43 @@
-package com.blog.mean.entity;
+package com.blog.application.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false, length = 10000)
+    private String content;
+    
+    private String excerpt;
+    
+    private String featuredImage;
+    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+    
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
