1572219f40d4114a535453fb8ba4a28378bb1db8
Maharaj P S <maharajpsd@gmail.com>
2026-03-17 15:19:37 +0530

    User Entity Added

--- mean/src/main/java/com/blog/mean/entity/User.java
+++ mean/src/main/java/com/blog/mean/entity/User.java
@@ -1,5 +1,36 @@
-package com.blog.mean.entity;
+package com.blog.application.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(nullable = false, unique = true)
+    private String username;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+    
+    private String bio;
+    
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 }
