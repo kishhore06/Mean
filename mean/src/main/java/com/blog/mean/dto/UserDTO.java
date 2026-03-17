a25595c36fc9a44221f74e41120a560dbd7e61c9
Mousigan Iyyamperumal <mousiganofficial@gmail.com>
2026-03-17 15:42:27 +0530

    dtos added

--- mean/src/main/java/com/blog/mean/dto/UserDTO.java
+++ mean/src/main/java/com/blog/mean/dto/UserDTO.java
@@ -1,5 +1,13 @@
 package com.blog.mean.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class UserDTO {
-    
+    private Long id;
+    private String username;
+    private String email;
+    private String bio;
 }
