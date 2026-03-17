c3e03747dc982ebb27f4e0cd55c27409d850d395
Mousigan Iyyamperumal <mousiganofficial@gmail.com>
2026-03-17 16:28:27 +0530

    exception, controller added

--- mean/src/main/java/com/blog/mean/controller/PostController.java
+++ mean/src/main/java/com/blog/mean/controller/PostController.java
@@ -1,5 +1,55 @@
 package com.blog.mean.controller;
 
+import com.blog.mean.dto.PostDTO;
+import com.blog.mean.dto.CreatePostRequest;
+import com.blog.mean.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequiredArgsConstructor
 public class PostController {
     
+    private final PostService postService;
+    
+    @PostMapping
+    public ResponseEntity<PostDTO> createPost(@RequestBody CreatePostRequest request) {
+        PostDTO createdPost = postService.createPost(request);
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<PostDTO>> getAllPosts() {
+        List<PostDTO> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> getPostById(@PathVariable Long id) {
+        PostDTO post = postService.getPostById(id);
+        return ResponseEntity.ok(post);
+    }
+    
+    @GetMapping("/author/{authorId}")
+    public ResponseEntity<List<PostDTO>> getPostsByAuthor(@PathVariable Long authorId) {
+        List<PostDTO> posts = postService.getPostsByAuthor(authorId);
+        return ResponseEntity.ok(posts);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDTO> updatePost(@PathVariable Long id, @RequestBody CreatePostRequest request) {
+        PostDTO updatedPost = postService.updatePost(id, request);
+        return ResponseEntity.ok(updatedPost);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return ResponseEntity.noContent().build();
+    }
 }
